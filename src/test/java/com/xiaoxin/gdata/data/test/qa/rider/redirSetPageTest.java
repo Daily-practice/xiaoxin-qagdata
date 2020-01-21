d536810a028f919b3a6b907a928d3902329f003a
liutianhua123 <15801631294@163.com>
2020-01-21 15:21:23 +0800

    call the method with the class name

--- src/test/java/com/xiaoxin/gdata/data/test/qa/rider/redirSetPageTest.java
+++ src/test/java/com/xiaoxin/gdata/data/test/qa/rider/redirSetPageTest.java
@@ -11,22 +11,21 @@ import java.io.File;
 
 public class redirSetPageTest {
     public static String host = HostUtils.getURL("rider-qa-apph5") ;
-    public static AssembledData rsp = new AssembledData() ;
     String path = "src"+ File.separator+"main"+File.separator+"resources"+ File.separator+"config"+ File.separator+"keyv.properties" ;
 
 
     @Test(retryAnalyzer = CustomRetry.class)
     public void queryHistroyOrder() {
-        rsp.bulkRequestInterface(ParamUtil.getProfile(path),"queryHistroyOrder",host, httpHeaders.riderAppHeader());
+        AssembledData.bulkRequestInterface(ParamUtil.getProfile(path),"queryHistroyOrder",host, httpHeaders.riderAppHeader());
     }
 
     @Test
     public void getBills() {
-        rsp.bulkRequestInterface(ParamUtil.getProfile(path),"getBills",host, httpHeaders.riderAppHeader());
+        AssembledData.bulkRequestInterface(ParamUtil.getProfile(path),"getBills",host, httpHeaders.riderAppHeader());
     }
 
     @Test
     public void getSch() {
-        rsp.bulkRequestInterface(ParamUtil.getProfile(path),"getSch",host, httpHeaders.riderAppHeader());
+        AssembledData.bulkRequestInterface(ParamUtil.getProfile(path),"getSch",host, httpHeaders.riderAppHeader());
     }
 }
